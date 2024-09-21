e9e12d7fa4667fbc1cbcc268f1deefd76c88532e
AbdulZor <abdulzor@outlook.com>
2024-09-21 09:31:44 +0200

    Add 16x16 squares into the grid with JS
    The height and width of the squares are calculated based on the
    height of the grid divided by 16 minus the border width of the grid.
    The border width of the grid is divided with 16 so each square gets a
    proportional cut that  makes it possible to fit 16 squares on a line.

---
+++ script.js
@@ -0,0 +1,32 @@
+// Problem: create a grid composed of 16x16 square divs
+// Plan: loop 16 times and add a new square to the grid.
+//       and have the height be (height/16) and width be 
+//       (width/16).
+// Pseudocode:
+//          OUTER LOOP i=0; i < 16; i++ (vertical squares)
+//              INNER LOOP i=0; i < 16; i++ (horizontal squares)
+//                  create new element 'div'
+//                  style height (grid.height/16 - 2px border)
+//                  style width (width.width/16 - 2px border)
+//                  style border black 1px solid
+//                  append element to grid node
+
+function createGrid() {
+    const grid = document.querySelector('.grid');
+    const gridSize = 16;
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let i = 0; i < gridSize; i++) {
+            const squareDiv = document.createElement('div');
+            const borderSize = getComputedStyle(grid, null).borderLeftWidth.slice(0, -2); // get only number (exclude 'px')
+
+            squareDiv.style.width = "" + (grid.offsetWidth / gridSize) - (borderSize * 2 / gridSize) + "px";
+            squareDiv.style.height = "" + (grid.offsetWidth / gridSize) - (borderSize * 2 / gridSize) + "px";
+            squareDiv.style.border = "solid 1px black";
+            
+            grid.appendChild(squareDiv);
+        }
+    }
+}
+
+createGrid();
